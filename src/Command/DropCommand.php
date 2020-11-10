840e2d71d8067704bdbe33622ed83067f1ea6f35
Valentin V / vvval <valentin@spiralscout.com>
2020-11-10 18:03:00 +0300

    remove meta

--- src/Command/DropCommand.php
+++ src/Command/DropCommand.php
@@ -6,34 +6,22 @@ namespace Spiral\WriteAway\Command;
 
 use Cycle\ORM\TransactionInterface;
 use Spiral\Console\Command;
-use Spiral\WriteAway\Repository\MetaRepository;
 use Spiral\WriteAway\Repository\PieceRepository;
 
 class DropCommand extends Command
 {
     protected const NAME        = 'writeaway:drop';
-    protected const DESCRIPTION = 'Drop all pieces and metas from the repository';
+    protected const DESCRIPTION = 'Drop all pieces from the repository';
 
-    public function perform(
-        PieceRepository $pieceRepository,
-        MetaRepository $metaRepository,
-        TransactionInterface $transaction
-    ): void {
+    public function perform(PieceRepository $pieceRepository, TransactionInterface $transaction): void
+    {
         $pieces = 0;
         foreach ($pieceRepository->findAll() as $piece) {
             $pieces++;
             $transaction->delete($piece);
         }
 
-        $metas = 0;
-        foreach ($metaRepository->findAll() as $meta) {
-            $metas++;
-            $transaction->delete($meta);
-        }
-
-        $this->output->writeln(
-            "Trying to delete <comment>$pieces</comment> pieces and <comment>$metas</comment> metas..."
-        );
+        $this->output->writeln("Trying to delete <comment>$pieces</comment> pieces...");
         try {
             $transaction->run();
         } catch (\Throwable $e) {
