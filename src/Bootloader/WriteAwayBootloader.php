da78cd0e9207c85ca3100ce3b568ac4489a3cd31
Valentin V / vvval <valentin@spiralscout.com>
2020-11-10 12:55:10 +0300

    add domain core with interceptors

--- src/Bootloader/WriteAwayBootloader.php
+++ src/Bootloader/WriteAwayBootloader.php
@@ -4,22 +4,29 @@ declare(strict_types=1);
 
 namespace Spiral\WriteAway\Bootloader;
 
-use Spiral\Boot\Bootloader\Bootloader;
+use Spiral\Bootloader\DomainBootloader;
 use Spiral\Config\ConfiguratorInterface;
+use Spiral\Core\CoreInterface;
+use Spiral\Domain\CycleInterceptor;
+use Spiral\Domain\FilterInterceptor;
 use Spiral\Keeper\Middleware\AccessMiddleware;
 use Spiral\Router\Bootloader\AnnotatedRoutesBootloader;
 use Spiral\Router\GroupRegistry;
 use Spiral\WriteAway\Config\WriteAwayConfig;
 
-class WriteAwayBootloader extends Bootloader
+class WriteAwayBootloader extends DomainBootloader
 {
     protected const DEPENDENCIES = [
         AnnotatedRoutesBootloader::class,
     ];
+    protected const INTERCEPTORS = [
+        CycleInterceptor::class,
+        FilterInterceptor::class
+    ];
 
     private const CONFIG = WriteAwayConfig::CONFIG;
 
-    public function boot(ConfiguratorInterface $config, GroupRegistry $groups): void
+    public function boot(ConfiguratorInterface $config, GroupRegistry $groups, CoreInterface $core): void
     {
         $config->setDefaults(
             self::CONFIG,
@@ -34,6 +41,7 @@ class WriteAwayBootloader extends Bootloader
         );
 
         $groups->getGroup('writeAway')
+            ->setCore($core)
             ->setPrefix($config->getConfig(self::CONFIG)['endpointPrefix'])
             ->addMiddleware(AccessMiddleware::class);
     }
