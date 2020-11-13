e870e894dde09b2fc789d8b5543bc8d79b4986cd
Valentin V / vvval <valentin@spiralscout.com>
2020-11-13 16:51:28 +0300

    add command test

---
+++ tests/CommandTest.php
@@ -0,0 +1,24 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Spiral\Tests\Writeaway;
+
+class CommandTest extends TestCase
+{
+    use HttpTrait;
+
+    /**
+     * @throws \Throwable
+     */
+    public function testDrop(): void
+    {
+        $this->assertCount(0, $this->repository()->select());
+
+        $this->post($this->uri('writeaway:pieces:save'), ['type' => 'piece', 'id' => 'name']);
+        $this->assertCount(1, $this->repository()->select());
+
+        $this->app->getConsole()->run('writeaway:drop');
+        $this->assertCount(0, $this->repository()->select());
+    }
+}
