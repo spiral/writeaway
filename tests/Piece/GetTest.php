2a88c2aa76d32b0eaf5dfb54ff708734b4798240
Valentin V / vvval <valentin@spiralscout.com>
2020-11-16 12:47:10 +0300

    move drop command test

--- tests/Piece/GetTest.php
+++ tests/Piece/GetTest.php
@@ -39,4 +39,19 @@ class GetTest extends TestCase
 
         $this->assertCount(0, $this->repository()->select());
     }
+
+    /**
+     * @throws \Throwable
+     */
+    public function testDropCommand(): void
+    {
+        $this->logSchema();
+        $this->assertCount(0, $this->repository()->select());
+
+        $this->post($this->uri('writeaway:pieces:save'), ['type' => 'piece', 'id' => 'name']);
+        $this->assertCount(1, $this->repository()->select());
+
+        $this->app->getConsole()->run('writeaway:drop');
+        $this->assertCount(0, $this->repository()->select());
+    }
 }
